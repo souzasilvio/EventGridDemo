470d3049714f34c99e866846f0e796932d817c3f
Silvio Jose de Souza <silvio.j.souza@gmail.com>
2019-08-27 22:25:01 -0300

    ajustes

--- EventWebConsumerHook1/Program.cs
+++ EventWebConsumerHook1/Program.cs
@@ -10,6 +10,9 @@ using Microsoft.Extensions.Logging;
 
 namespace EventWebHookConsumer1
 {
+    /// <summary>
+    /// Exemplo de subscriber 1
+    /// </summary>
     public class Program
     {
         public static void Main(string[] args)
